bb9458a7e27f6e55df404e4dc84c670549e9dfa3
sallaben <steven.allaben@gmail.com>
2019-04-12 19:00:23 -0400

    surprise

--- rec.py
+++ rec.py
@@ -1,60 +1,33 @@
 #!/Users/sallaben/anaconda3/bin/python
 print("LOADING LIBRARIES...")
-from lenskit import batch, topn, util
-from lenskit import crossfold as xf
-from lenskit.algorithms import Recommender, als, user_knn as knn
-from lenskit import topn
+from surprise import KNNBasic
+from surprise import Reader
+from surprise.model_selection import cross_validate
+from surprise import Dataset
+from surprise import SVD
 import pandas as pd
 
 print("LOADING DATASET...")
-#ratings = pd.read_csv("lastfm-dataset-360K/usersha1-artmbid-artname-plays.tsv", sep="\t",
-#                      names = ['user', 'artistid', 'artist', 'plays'], header = None)
-#profiles = pd.read_csv("lastfm-dataset-360K/usersha1-profile.tsv", sep="\t",
-#                      names=['user', 'gender', 'age', 'country', 'signup'], header=None)
-ratings = pd.read_csv("lastfm-dataset-360K/obsession3.tsv", sep="\t",
+ratings = pd.read_csv("lastfm-dataset-360K/o0.tsv", sep="\t",
                     names=['user', 'item', 'plays', 'rating'], header=None)
-# ratings = ratings.drop(ratings.index[[12520108,14801833]])
 
-#algo_ii = knn.ItemItem(50)
-#algo_uu = knn.UserUser(50)
-algo_als = als.BiasedMF(50)
+scale = Reader(rating_scale=(0, 1))
+data = Dataset.load_from_df(ratings[['user', 'item', 'rating']], scale)
 
-# print("SUMMING PLAYS BY USER...")
-# plays = ratings.groupby(['user'], as_index=False)[['user', 'plays']].sum()
-# plays.reset_index()
-# playdict = dict(zip(plays["user"], plays["plays"]))
+print("BUILDING TRAINSET...")
+trainset = data.build_full_trainset()
+print("FITTING ALGORITHM...")
+algo = KNNBasic()
+algo.fit(trainset)
 
-# print("CALCULATING LEVEL OF OBSESSION...")
-# obsession = []
-# for index, row in ratings.iterrows():
-#     if (index % 100000) == 0:
-#         print(index)
-#     obsession.append(round(float(row["plays"] / playdict[row["user"]]), 5))
+# raw user id (as in the ratings file). They are **strings**!
+uid = "000063d3fe1cf2ba248b9e3c3f0334845a27a6bf"
+iid = "nujabes"  # raw item id (as in the ratings file). They are **strings**!
 
-# print("EXPORTING TO CSV...")
-# ratings = ratings.assign(o=obsession)
-# del ratings["artistid"]
-# ratings.to_csv("lastfm-dataset-360K/obsession3.tsv", sep="\t", header=None)
+# get a prediction for specific users and items.
+print("PREDICTING...")
+pred = algo.predict(uid, iid, verbose=True)
 
-def eval(aname, algo, train, test):
-    fittable = util.clone(algo)
-    fittable = Recommender.adapt(fittable)
-    fittable.fit(train)
-    users = test.user.unique()
-    # now we run the recommender
-    recs = batch.recommend(fittable, users, 25)
-    # add the algorithm name for analyzability
-    recs['Algorithm'] = aname
-    return recs
-
-print("FITTING AND EVALUATING...")
-all_recs = []
-test_data = []
-for train, test in xf.partition_users(ratings[['user', 'item', 'rating']], 5, xf.SampleFrac(0.2)):
-    test_data.append(test)
-    #all_recs.append(eval('UserUser', algo_uu, train, test))
-    #all_recs.append(eval('ItemItem', algo_ii, train, test))
-    all_recs.append(eval('ALS', algo_als, train, test))
-all_recs = pd.concat(all_recs, ignore_index=True)
-
-print(all_recs.head())
+print("CROSS VALIDATION...")
+algo = SVD()
+cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
